8e61c3393d49b21efc54ec19cb7242cd5372def6
Namrath02 <82800026+Namrath02@users.noreply.github.com>
2021-04-27 21:42:13 +0530

    Update ok.c

--- ok.c
+++ ok.c
@@ -1,22 +1,3 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
-#include<math.h>
-
-struct __Question__
-{
-    int hashq;              // stores the hash value of the question text
-    char * type;            // defaults to MCQ for version 1 (type = "MCQ")
-    char * text;            // has the text of the question
-    double difficulty;      // stores the difficulty level of the question (max = 1)
-    int no_options;         // stores the number of options for the question
-    char ** options;        // contains the options in an array of strings
-    char * correct_option;  // stores the correct option of the question
-};
-
-typedef struct __Question__ Question_B; 
 
    // PSEUDOCODE //
     // while(reading lines):
